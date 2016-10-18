11e51d449428ac60880da8c4b8bc0b2c73a8c030
Zhiyong Yang <coffeeisland798@gmail.com>
2016-10-18 22:20:44 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,32 +2,27 @@
 class MoviesController < ApplicationController
 
   def index
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @all_ratings = ['NC-17', 'G', 'PG', 'PG-13', 'R']
 
-    session[:ratings] = params[:ratings] if params[:ratings]
-    session[:sort]    = params[:sort]    if params[:sort]
-
-    if session[:ratings] || session[:sort]
-      case session[:sort]
+    if params[:sort]||params[:ratings]
+      case params[:sort]
       when 'title'
         @title_hilite = 'hilite'
+       @movies = Movie.order(params[:sort]) 
       when 'release_date'
         @release_hilite = 'hilite'
+        @movies = Movie.order(params[:sort])
       end
-
-      session[:ratings] ||= @all_ratings
-      @ratings = session[:ratings]
+      case params[:ratings]
+      when params[:ratings] ||= @all_ratings
+      @ratings = params[:ratings]
       @ratings = @ratings.keys if @ratings.respond_to?(:keys)
-      @movies = Movie.find(:all,
-                           order: session[:sort],
-                           conditions: ["rating IN (?)", @ratings])
+      @movies = Movie.where({rating: @ratings})
+      end
     else
-      @movies = Movie.all
+      @movies=Movie.all
     end
 
-    if session[:ratings] != params[:ratings] || session[:sort] != params[:sort]
-      redirect_to movies_path(ratings: session[:ratings], sort: session[:sort])
-    end
   end
 
   def show
